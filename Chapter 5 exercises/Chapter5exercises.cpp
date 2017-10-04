8f1a3f0da66fa4c48507934ddf1d37477012b4e0
Das KuG <32452916+AlphaMorgothist@users.noreply.github.com>
2017-10-03 23:34:58 -0400

    Add files via upload

---
+++ Chapter 5 exercises/Chapter5exercises.cpp
@@ -0,0 +1,107 @@
+// Chapter5exercises.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+
+// Hangman
+// The classic game of hangman
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ctime>
+#include <cctype>
+
+using namespace std;
+char getInput();
+void checkInput(string &used, string &words, string &soFar, int &wrong, char &guess);
+
+const int MAX_WRONG = 8;
+
+int main()
+{
+	// set-up
+	  // maximum number of incorrect guesses allowed
+
+	vector<string> words;  // collection of possible words to guess
+	words.push_back("GUESS");
+	words.push_back("HANGMAN");
+	words.push_back("DIFFICULT");
+
+	srand(static_cast<unsigned int>(time(0)));
+	random_shuffle(words.begin(), words.end());
+	const string THE_WORD = words[0];            // word to guess
+	int wrong = 0;                               // number of incorrect guesses
+	string soFar(THE_WORD.size(), '-');          // word guessed so far
+	string used = "";                            // letters already guessed
+	
+
+	cout << "Welcome to Hangman.  Good luck!\n";
+
+	// main loop
+	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+	{
+		cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guesses left.\n";
+		cout << "\nYou've used the following letters:\n" << used << endl;
+		cout << "\nSo far, the word is:\n" << soFar << endl;
+		char guess = getInput();
+		checkInput(used, words[0], soFar, wrong, guess);
+	}
+	return 0;
+	}
+
+char getInput() {
+	char guess;
+	cout << "\n\nEnter your guess: ";
+	cin >> guess;
+	guess = toupper(guess); //make uppercase since secret word in uppercase
+	return guess;
+}
+
+void checkInput(string &used, string &words, string &soFar, int &wrong, char &guess)
+{
+	string THE_WORD = words;
+	while (used.find(guess) != string::npos)
+	{
+		cout << "\nYou've already guessed " << guess << endl;
+		cin >> guess;
+	}
+
+	used += guess;
+
+	if (THE_WORD.find(guess) != string::npos)
+	{
+		cout << "That's right! " << guess << " is in the word.\n";
+
+		// update soFar to include newly guessed letter
+		for (unsigned int i = 0; i < THE_WORD.length(); ++i)
+		{
+			if (THE_WORD[i] == guess)
+			{
+				soFar[i] = guess;
+			}
+		}
+	}
+	else
+	{
+		cout << "Sorry, " << guess << " isn't in the word.\n";
+		++wrong;
+	}
+
+
+	// shut down
+	if (wrong == MAX_WRONG) {
+		cout << "\nYou've been hanged!";
+		system("pause");
+	}
+	else if (soFar == THE_WORD) {
+		cout << "\nYou guessed it!";
+		cout << "\nThe word was " << THE_WORD << endl;
+		system("pause");
+	}
+	}
+
